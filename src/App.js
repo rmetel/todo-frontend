e7b861b2aa549d794a9a34a24d4aa5328b24365f
Ralph Metel <ralph.metel@exxeta.com>
2022-09-12 17:50:42 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+    Button,
+    Col,
+    Container,
+    Form,
+    InputGroup,
+    ListGroup,
+    Row
+} from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const tasks = [
+        {
+            "id": 1,
+            "description": "Milch kaufen"
+        },
+        {
+            "id": 2,
+            "description": "Essen zubereiten"
+        }
+    ]
+    return (
+        <div className="App">
+            <Container className="mt-4">
+                <Row>
+                    <Col className={"col-lg-8 offset-lg-2"}>
+                        <div className={"text-center mb-4"}>
+                            <h1>ToDo List</h1>
+                        </div>
+                        <InputGroup className="mb-3">
+                            <Form.Control
+                                placeholder="New task"
+                                aria-label="ToDo"
+                                aria-describedby="new task"
+                            />
+                            <Button variant="outline-secondary" id="button-add">
+                                Add
+                            </Button>
+                        </InputGroup>
+
+                        <ListGroup>
+                            {tasks.map(task =>
+                                <ListGroup.Item key={task.id}>{task.description}</ListGroup.Item>
+                            )}
+                        </ListGroup>
+
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
