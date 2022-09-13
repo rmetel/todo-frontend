7c0a9517c2b4eeb423934e9078bfefa6e7a8b94f
Ralph Metel <ralph.metel@exxeta.com>
2022-09-13 18:29:24 +0200

    refactored api url logic, added crud methods, added alert component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import {
+    Alert,
     Button,
     Col,
     Container,
@@ -9,32 +10,64 @@ import {
     ListGroup,
     Row
 } from "react-bootstrap";
+import axios from 'axios';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.getTasks = this.getTasks.bind(this);
+
+        let isLocal = window.location.href.indexOf("localhost") > -1;
+        let localHost = "http://localhost:8080";
+        let apiEndpoint = "/tasks";
+
+        this.apiUrl = isLocal? localHost + apiEndpoint : apiEndpoint;
+        this.getAll = this.getAll.bind(this);
+        this.add = this.add.bind(this);
         this.state = {tasks: []};
+
     }
 
     componentDidMount() {
-        this.getTasks();
+        this.getAll();
     }
 
-    getTasks() {
-        let isLocal = window.location.href.indexOf("localhost") > -1;
-        let urlLocal = "http://localhost:8080";
-        let endPoint = "/tasks/all";
-
-        fetch(isLocal? urlLocal + endPoint : endPoint)
+    getAll() {
+        fetch(this.apiUrl + "/all")
             .then(response => response.json())
             .then(data => this.setState({tasks: data}));
+    }
+
+    add() {
+        const getAll = this.getAll;
+
+        let alertBox = document.querySelector("[role=alert]");
+
+        let params = {
+            description: document.getElementById(`description`).value
+        };
+
+        axios.post(this.apiUrl + "/add", params)
+            .then(function (response) {
+                if(response.status === 200) {
+                    alertBox.innerHTML = `Task wurde erstellt!`;
+                    alertBox.classList.remove("d-none");
+                    window.setTimeout(function() {
+                        alertBox.classList.add("d-none");
+                    }, 2000);
+
+                    getAll();
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     render() {
         const {tasks} = this.state;
         return (
             <div className="App">
+                <Alert variant="success" className="d-none"></Alert>
                 <Container className="mt-4">
                     <Row>
                         <Col className={"col-lg-8 offset-lg-2"}>
@@ -43,11 +76,12 @@ class App extends React.Component {
                             </div>
                             <InputGroup className="mb-3">
                                 <Form.Control
+                                    id={"description"}
                                     placeholder="New task"
                                     aria-label="ToDo"
                                     aria-describedby="new task"
                                 />
-                                <Button variant="outline-secondary" id="button-add">
+                                <Button variant="outline-secondary" id="button-add" onClick={this.add}>
                                     Add
                                 </Button>
                             </InputGroup>
