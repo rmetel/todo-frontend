3d9d9c6e7e2a3903409e9a3f2cca689a1f818d92
Ralph Metel <ralph.metel@exxeta.com>
2022-09-13 16:08:33 +0200

    rebuilt App.js from function to React.Component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import {
     Button,
@@ -9,51 +10,60 @@ import {
     Row
 } from "react-bootstrap";
 
-function App() {
-    const tasks = [
-        {
-            "id": 1,
-            "description": "Milch kaufen"
-        },
-        {
-            "id": 2,
-            "description": "Essen zubereiten"
-        },
-        {
-            "id": 3,
-            "description": "Buch lesen"
-        }
-    ]
-    return (
-        <div className="App">
-            <Container className="mt-4">
-                <Row>
-                    <Col className={"col-lg-8 offset-lg-2"}>
-                        <div className={"text-center mb-4"}>
-                            <h1>ToDo List</h1>
-                        </div>
-                        <InputGroup className="mb-3">
-                            <Form.Control
-                                placeholder="New task"
-                                aria-label="ToDo"
-                                aria-describedby="new task"
-                            />
-                            <Button variant="outline-secondary" id="button-add">
-                                Add
-                            </Button>
-                        </InputGroup>
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.getTasks = this.getTasks.bind(this);
+        this.state = {tasks: []};
+    }
 
-                        <ListGroup as="ol" numbered>
-                            {tasks.map(task =>
-                                <ListGroup.Item key={task.id}>{task.description}</ListGroup.Item>
-                            )}
-                        </ListGroup>
+    componentDidMount() {
+        this.getTasks();
+    }
 
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+    getTasks() {
+        let isLocal = window.location.href.indexOf("localhost") > -1;
+        let urlLocal = "http://localhost:8080";
+        let endPoint = "/tasks/all";
+
+        fetch(isLocal? urlLocal + endPoint : endPoint)
+            .then(response => response.json())
+            .then(data => this.setState({tasks: data}));
+    }
+
+    render() {
+        const {tasks} = this.state;
+        return (
+            <div className="App">
+                <Container className="mt-4">
+                    <Row>
+                        <Col className={"col-lg-8 offset-lg-2"}>
+                            <div className={"text-center mb-4"}>
+                                <h1>ToDo List</h1>
+                            </div>
+                            <InputGroup className="mb-3">
+                                <Form.Control
+                                    placeholder="New task"
+                                    aria-label="ToDo"
+                                    aria-describedby="new task"
+                                />
+                                <Button variant="outline-secondary" id="button-add">
+                                    Add
+                                </Button>
+                            </InputGroup>
+
+                            <ListGroup as="ol" numbered>
+                                {tasks.map(task =>
+                                    <ListGroup.Item key={task.id}>{task.description}</ListGroup.Item>
+                                )}
+                            </ListGroup>
+
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default App;
