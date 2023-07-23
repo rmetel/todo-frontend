3605470dc1c93d8b41ca9096dd6ca03356d6f3fe
ralph.metel@gmx.de <pOison_2(>
2023-07-24 01:58:41 +0200

    added styling to PrivateRoute.tsx

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useKeycloak } from "@react-keycloak/web";
+import { Col, Row } from "react-bootstrap";
 
 interface PrivateRouteProps {
   children: React.ReactElement | null;
@@ -12,21 +13,30 @@ export const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
 
   return (
     <>
-      {isLoggedIn ? children : null}
-
-      <div style={{ border: "1px solid #777", borderRadius: 3, padding: 10, textAlign: "center", margin: "20px 0" }}>
-        <p>This is a Keycloak-secured component of your application.</p>
-
-        {isLoggedIn ?
-          <button type="submit" className="btn btn-primary" onClick={() => keycloak.logout()}>
-            Logout
-          </button>
-          :
-          <button type="submit" className="btn btn-primary" onClick={() => keycloak.login()}>
-            Login
-          </button>
-        }
+      <div style={{
+        border: "1px solid #777",
+        borderRadius: 3,
+        padding: 10,
+        margin: "20px 0",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center"
+      }}>
+        <div>This is a Keycloak-secured component of your application.</div>
+        <div>
+          {isLoggedIn ?
+            <button type="submit" className="btn btn-primary" onClick={() => keycloak.logout()}>
+              Logout
+            </button>
+            :
+            <button type="submit" className="btn btn-primary" onClick={() => keycloak.login()}>
+              Login
+            </button>
+          }
+        </div>
       </div>
+
+      {isLoggedIn ? children : null}
     </>
   );
 };
