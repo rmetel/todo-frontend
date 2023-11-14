2f0ae0df61ff4e4d90bce3740b5fcc3a50fa3937
01000982 <ralph.metel.extern@s-management-services.de>
2023-11-14 12:46:08 +0100

    updated base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/",
+  base: "http://todo-app.tech/",
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src")
