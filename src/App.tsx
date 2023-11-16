84f9cfeadac46a63bab566780ee7c9d26ff00123
ralph.metel@gmx.de <pOison_2(>
2023-11-17 00:29:52 +0100

    synced frontend version

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ const App = () => {
               </Router>
               <h6 id={"apiVersion"}>
                 <Badge bg="info" className="mb-1">
-                  fe: main 1.3.113
+                  fe: main 1.3.115
                 </Badge>
                 <br />
                 <Badge bg="secondary">
