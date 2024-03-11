28fbd51fcee3a807497ac2c8b9b7f39b8751d174
ralph.metel@gmx.de <pOison_2(>
2024-03-11 21:50:24 +0100

    testing

--- src/auth/keycloak.ts
+++ src/auth/keycloak.ts
@@ -2,7 +2,7 @@ import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
   // url: "http://localhost:8080/",
-  url: "/keycloak",
+  url: "https://todo-app.tech:8443",
   realm: "todo-app",
   clientId: "todo-app",
 });
